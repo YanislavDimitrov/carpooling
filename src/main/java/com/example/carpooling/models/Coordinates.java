7e4d3baad942cf5520bd5e863cec9d9c6fa229e2
b0ew <iboew9@gmail.com>
2023-08-09 17:30:53 +0300

    Introducing travel and coordinate entitites

--- src/main/java/com/example/carpooling/models/Coordinates.java
+++ src/main/java/com/example/carpooling/models/Coordinates.java
@@ -1,2 +1,10 @@
-package com.example.carpooling.models;public class Coordinates {
+package com.example.carpooling.models;
+
+public class Coordinates {
+    @Embedded
+    @AttributeOverrides( {
+            @AttributeOverride(name = "latitude", column = @Column(name = "start_latitude")),
+            @AttributeOverride(name = "longitude", column = @Column(name = "start_longitude"))
+    })
+    private Coordinates startPoint;
 }
