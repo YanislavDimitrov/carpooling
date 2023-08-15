24e326d6d871a6f94203a0400d27fb70bfaa036a
b0ew <iboew9@gmail.com>
2023-08-15 20:07:26 +0300

    adding new insert scripts to refactor the duration time in valid minutes for the travel view dto.

---
+++ src/main/resources/db/migration/V8__insertScriptWithValidDuration.sql
@@ -0,0 +1,20 @@
+use carpooling ;
+update travels
+set duration = '124.3 minutes'
+where duration = '3hours 54 minutes';
+
+update travels
+set duration = '114.3 minutes'
+where duration = '1hours 54 minutes';
+
+update travels
+set duration = '104.3 minutes'
+where duration = '1hours 14 minutes';
+
+update travels
+set duration = '144.5 minutes'
+where duration = '2hours 14 minutes';
+
+update travels
+set duration = '194.3 minutes'
+where duration = '3hours 54 minutes'
---
+++ src/main/resources/db/migration/V9__insertScriptWithMinutes.sql
@@ -0,0 +1,4 @@
+use carpooling;
+update travels
+set duration = '143.4 minutes'
+where duration = ' 30 minutes'
