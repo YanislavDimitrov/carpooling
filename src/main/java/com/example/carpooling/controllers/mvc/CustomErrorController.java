5c937f0537e5f287dc32f487cf3b43a99a8968e5
Yanislav Dimitrov <yanislav.dimitrov1994@bav.bg>
2023-09-05 14:27:30 +0300

    Adding @Controller annotation to CustomErrorController

--- src/main/java/com/example/carpooling/controllers/mvc/CustomErrorController.java
+++ src/main/java/com/example/carpooling/controllers/mvc/CustomErrorController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
